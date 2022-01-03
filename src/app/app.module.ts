0a45de1ef5c7203fa176137594279dd246ce8ba5
Brejcha13320 <davidmejia-2000@outlook.com>
2022-01-03 00:40:25 -0500

    se crearon los componentes de las paginas principales

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/login/login.component';
+import { HomeComponent } from './pages/home/home.component';
+import { RegistroComponent } from './pages/registro/registro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    RegistroComponent
   ],
   imports: [
     BrowserModule
